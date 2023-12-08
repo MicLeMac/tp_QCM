5e3c879ff7ebeeb4248e617de68d39ce7bdf2354
MicLeMac <98753816+MicLeMac@users.noreply.github.com>
2023-12-08 15:24:04 +0100

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -9,7 +9,7 @@ size_of_board = 600
 symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
 symbol_thickness = 50
 symbol_X_color = '#EE4035'
-symbol_O_color = '#0492CF
+symbol_O_color = '#0492C
 
 
 class Tic_Tac_Toe():
